fbc849d93ae2688d1571b28a6d164d053aac41a4
Ömer Ulusal <96357374+omerulusal@users.noreply.github.com>
2024-01-18 22:59:57 +0300

    Pages

--- MoviesApp/ViewComponents/GenresViewComponent.cs
+++ MoviesApp/ViewComponents/GenresViewComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
 using MoviesApp.Data;
 using MoviesApp.Models;
 
@@ -11,13 +13,15 @@ public class GenresViewComponent : ViewComponent
     private readonly MovieContext _context;
     public GenresViewComponent(MovieContext context)
     {
-        _context = context;
+        _context = context;//dbden gelen icerigi _contexte atadım
     }
 
 
     public IViewComponentResult Invoke()
     {
         ViewBag.SelectedGenre = RouteData?.Values["genre"];
+        //route verileri içindeki "genre" değerini ViewBag.SelectedGenre özelliğine atar
         return View(_context.Genres.ToList());
+        //_context üzerinden tüm film türlerini alır ve bir view'a geçirir. 
     }
 }
